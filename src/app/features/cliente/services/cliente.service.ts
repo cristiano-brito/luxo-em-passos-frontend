9ab4c0109a7339f1129b0266850938785d5d152e
cristiano-brito <cristianocomputacao@hotmail.com>
2026-02-02 02:09:42 -0300

    feat(cliente): integra listagem com API real e mapeamento de DTO
    Migração do consumo de dados estáticos para HttpClient, utilizando a estrutura de ApiResponse e tratamento de dados aninhados para Clientes e Endereços.

--- src/app/features/cliente/services/cliente.service.ts
+++ src/app/features/cliente/services/cliente.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
 import { LuxoService } from '../../../services/luxo.service';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Cliente } from '../../../models/luxo.models';
+import { HttpClient } from '@angular/common/http';
+import { ApiResponse } from '../../../models/api-response.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClienteService {
+  private readonly API = 'api/clientes';
 
-  constructor(private luxoData: LuxoService) {}
+  constructor(
+    private luxoData: LuxoService,
+    private http: HttpClient,
+  ) {}
 
   listarTodos(): Observable<Cliente[]> {
-    // Busca os clientes do banco fake central
-    return this.luxoData.getClientes();
+    return this.http.get<ApiResponse<Cliente[]>>(this.API).pipe(
+      map((res) => res.dados),
+    );
   }
 
   adicionar(cliente: Cliente): Observable<Cliente> {
